9f40496600d337e9b21b5f3f8cf8d3fc460191b3
amainooti <amainooti@gmail.com>
2023-02-03 00:33:06 +0100

    added flask wtf and wtf forms and connected the flask app to the database

--- market/__init__.py
+++ market/__init__.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config["SECRET_KEY"] = 'c53b42c66cbcd0c8796d741f'
 db = SQLAlchemy(app)
 
 from market import routes
